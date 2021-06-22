846bfaf33e46b77d62a37663b2a1a487b015ee29
shawlycode <orlandoshawly>
2021-06-22 14:45:46 +0000

    add component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import ClassCounter from './ClassCounter';
+import FunctionCounter from './FunctionCounter';
 
 function App() {
   return (
     <div className="App">
      <ClassCounter />
+     <br></br>
+     <hr></hr>
+     <FunctionCounter />
     </div>
   );
 }
